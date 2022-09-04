ebf407f4ee929f4d7300ccfaafea2a2d977ef454
Misael Carriera <79615851+RighteousHeroM@users.noreply.github.com>
2022-09-04 01:45:23 -0400

    Update Dice_Roll.py

--- Dice_Roll.py
+++ Dice_Roll.py
@@ -3,7 +3,7 @@ import random
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
 
-print(dice1)
-print(dice2)
+print("Dice one = ", dice1)
+print("Dice two = ",dice2)
 
-print(dice1 + dice2)
+print("Combined Total = ",dice1 + dice2)
